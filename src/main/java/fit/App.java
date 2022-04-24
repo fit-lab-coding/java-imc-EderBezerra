2a9f5fefcf54cf76ada43e021cbb2e581348d4e3
Eder <eder992@gmail.com>
2022-04-23 23:52:26 -0300

    Novo teste

--- src/main/java/fit/App.java
+++ src/main/java/fit/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )    
     {
-      System.out.println(CalculadoraImc.obterIndice(1.7, 110));
+      System.out.println(CalculadoraImc.obterIndice(1.7, 100));
 
     }
 }
